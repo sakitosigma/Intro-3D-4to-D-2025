7ea549dab81c0c343cfcad386843113e8975d11c
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2025-05-12 10:52:03 -0300

    Initial commit

---
+++ Assets/RandomCloner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCloner : MonoBehaviour
+{
+    public int randomNumber;
+    public int min;
+    public int max;
+
+    public GameObject prefab1;
+    public GameObject prefab2;
+    public GameObject prefab3;
+
+    public void CloneRandom()
+    {
+        randomNumber = Random.Range(min,max + 1);
+        GameObject clone = prefab1;
+        if (randomNumber == 0)
+        {
+            clone = prefab1;
+        }
+        else if (randomNumber == 1)
+        {
+            clone = prefab2;
+        }
+        else if (randomNumber == 2)
+        {
+            clone = prefab3;
+        }
+
+        Instantiate(clone);
+
+    }
+}
---
+++ Assets/Scripts/Instantiator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+    public GameObject cubePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ClonarCubo();
+    }
+
+    public void ClonarCubo()
+    {
+        Instantiate(cubePrefab);
+    }
+
+}
