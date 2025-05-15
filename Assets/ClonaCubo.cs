ea9d9cd393c4d93daa877a59b275e1a46b89b114
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2025-05-15 10:39:36 -0300

    FPS Parkour respwn on death

---
+++ Assets/ClonaCubo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClonaCubo : MonoBehaviour
+{
+    public GameObject cuboPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ClonarCubo();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ClonarCubo()
+    {
+        Instantiate(cuboPrefab);
+    }
+}
---
+++ Assets/DeathRespawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathRespawn : MonoBehaviour
+{
+    public float deathHeight = -20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < deathHeight)
+        {
+            Debug.Log("Respawn");
+            SceneManager.LoadScene("FPS parkour");
+        }
+    }
+}
