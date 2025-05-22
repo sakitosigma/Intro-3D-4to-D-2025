d248943da2442f6b28bdb7db9328c479ba652258
micabod <167199490+micabod@users.noreply.github.com>
2025-05-22 13:57:29 -0300

    Colisiones

---
+++ Assets/DeteccionDeColisiones.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeteccionDeColisiones : MonoBehaviour
+{
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Contacto");
+        if (col.gameObject.name == "Player")
+        {
+            Destroy(gameObject);
+        }        
+    }
+}
