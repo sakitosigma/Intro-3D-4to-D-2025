0f0974a155bc9264b8170e0dabfa64284d8d8e14
micabod <jeronimosanchezperuga@gmail.com>
2025-06-09 10:52:16 -0300

    Time Invoke Coroutine

---
+++ Assets/ExecutionOrder.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExecutionOrder : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log("Awake");
+    }
+    void Start()
+    {
+        Debug.Log("Start");        
+    }
+    void Update()
+    {
+        Debug.Log("Update");
+    }
+    void FixedUpdate()
+    {
+        Debug.Log("FixedUpdate");
+    }
+    void OnEnable()
+    {
+        Debug.Log("OnEnable");
+    }
+}
---
+++ Assets/WaitNSeconds.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitNSeconds : MonoBehaviour
+{
+    public float timeToWait;
+    float timeElapsed;
+    public Transform cuboTR;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //InvokeRepeating(nameof(ElevateCube),0,timeToWait);}
+        StartCoroutine(WaitUntilElevate());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {             
+        //if (timeElapsed >= timeToWait)
+        //{
+        //    Debug.Log("Tiempo cumplido");
+        //    ElevateCube();
+        //    timeElapsed = 0;
+        //}
+        //else
+        //{
+        //    Debug.Log(timeElapsed += Time.deltaTime);
+        //}
+    }
+
+    IEnumerator WaitUntilElevate()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(timeToWait);
+            ElevateCube();
+        }
+    }
+    void ElevateCube()
+    {
+        cuboTR.position += Vector3.up * 2;
+    }
+}
